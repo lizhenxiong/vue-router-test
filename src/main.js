d0c6eee64200ae3f4b8673a428de2b9bd781a567
lizhenxiong <lizhenxiong@howzhi.com>
2018-04-04 16:18:13 +0800

    动态路由

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ Vue.use(VueRouter)
 
 const Foo = { template: '<div>Foo</div>' }
 const Bar = { template: '<div>Bar</div>' }
+const User = { template: '<div>User {{ $route.params.id }} </div>' }
 
 const routes = [
   { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/bar', component: Bar },
+  { path: '/user/:id', component: User }
 ]
 
 const router = new VueRouter({
